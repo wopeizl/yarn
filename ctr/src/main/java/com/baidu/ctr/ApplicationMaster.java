7b05a75050a94993ff1a38731a689b11c02a860d
peizhilin <wopeizl@163.com>
2019-03-05 14:30:32 +0800

    add the am client

---
+++ ctr/src/main/java/com/baidu/ctr/ApplicationMaster.java
@@ -0,0 +1,246 @@
+package com.baidu.ctr;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.net.NetUtils;
+import org.apache.hadoop.security.UserGroupInformation;
+import org.apache.hadoop.yarn.api.ApplicationConstants;
+import org.apache.hadoop.yarn.api.protocolrecords.RegisterApplicationMasterResponse;
+import org.apache.hadoop.yarn.api.records.ApplicationAttemptId;
+import org.apache.hadoop.yarn.api.records.Container;
+import org.apache.hadoop.yarn.api.records.ContainerId;
+import org.apache.hadoop.yarn.api.records.ContainerLaunchContext;
+import org.apache.hadoop.yarn.api.records.ContainerStatus;
+import org.apache.hadoop.yarn.api.records.FinalApplicationStatus;
+import org.apache.hadoop.yarn.api.records.NodeReport;
+import org.apache.hadoop.yarn.api.records.Priority;
+import org.apache.hadoop.yarn.api.records.Resource;
+import org.apache.hadoop.yarn.api.records.timeline.TimelineEntity;
+import org.apache.hadoop.yarn.api.records.timeline.TimelineEvent;
+import org.apache.hadoop.yarn.api.records.timeline.TimelinePutResponse;
+import org.apache.hadoop.yarn.client.api.AMRMClient.ContainerRequest;
+import org.apache.hadoop.yarn.client.api.TimelineClient;
+import org.apache.hadoop.yarn.client.api.async.AMRMClientAsync;
+import org.apache.hadoop.yarn.client.api.async.NMClientAsync;
+import org.apache.hadoop.yarn.client.api.async.impl.NMClientAsyncImpl;
+import org.apache.hadoop.yarn.exceptions.YarnException;
+import org.apache.hadoop.yarn.util.ConverterUtils;
+
+public class ApplicationMaster {
+	private final AtomicInteger sleepSeconds = new AtomicInteger(0);
+	private class LaunchContainerTask implements Runnable {
+		Container container;
+		public LaunchContainerTask(Container container) {
+			this.container = container;
+		}
+		public void run() {
+			List<String> commands = new LinkedList<String>();
+			//commands.add("sleep " + sleepSeconds.addAndGet(1));
+			commands.add(" ls ");
+			ContainerLaunchContext ctx = ContainerLaunchContext.newInstance(
+					null, null, commands, null, null, null);
+			amNMClient.startContainerAsync(container, ctx);
+		}
+	}
+
+	private class RMCallbackHandler implements AMRMClientAsync.CallbackHandler {
+		
+		
+
+		public void onContainersCompleted(List<ContainerStatus> statuses) {
+			for (ContainerStatus status : statuses) {
+				LOG.info("Container Completed: " + status.getContainerId().toString() 
+						+ " exitStatus="+ status.getExitStatus());
+				if (status.getExitStatus() != 0) {
+					// restart
+				}
+				ContainerId id = status.getContainerId();
+				runningContainers.remove(id);
+				numCompletedConatiners.addAndGet(1);
+			}
+		}
+
+		public void onContainersAllocated(List<Container> containers) {
+			for (Container c : containers) {
+				LOG.info("Container Allocated"
+						+ ", id=" + c.getId() 
+						+ ", containerNode=" + c.getNodeId());
+				exeService.submit(new LaunchContainerTask(c));
+				runningContainers.put(c.getId(), c);
+			}
+		}
+
+		public void onShutdownRequest() {
+		}
+
+		public void onNodesUpdated(List<NodeReport> updatedNodes) {
+
+		}
+
+		public float getProgress() {
+			float progress = 0;
+			return progress;
+		}
+
+		public void onError(Throwable e) {
+			amRMClient.stop();
+		}
+
+	}
+
+	private class NMCallbackHandler implements NMClientAsync.CallbackHandler {
+
+		public void onContainerStarted(ContainerId containerId,
+				Map<String, ByteBuffer> allServiceResponse) {
+			LOG.info("Container Stared " + containerId.toString());
+
+		}
+
+		public void onContainerStatusReceived(ContainerId containerId,
+				ContainerStatus containerStatus) {
+
+		}
+
+		public void onContainerStopped(ContainerId containerId) {
+			// TODO Auto-generated method stub
+
+		}
+
+		public void onStartContainerError(ContainerId containerId, Throwable t) {
+			// TODO Auto-generated method stub
+
+		}
+
+		public void onGetContainerStatusError(ContainerId containerId,
+				Throwable t) {
+			// TODO Auto-generated method stub
+
+		}
+
+		public void onStopContainerError(ContainerId containerId, Throwable t) {
+			// TODO Auto-generated method stub
+
+		}
+
+	}
+
+
+	
+	
+	@SuppressWarnings("rawtypes")
+	AMRMClientAsync amRMClient = null;
+	NMClientAsyncImpl amNMClient = null;
+	
+	AtomicInteger numTotalContainers = new AtomicInteger(1);
+	AtomicInteger numCompletedConatiners = new AtomicInteger(0);
+	ExecutorService exeService = Executors.newCachedThreadPool();
+	Map<ContainerId, Container> runningContainers = new ConcurrentHashMap<ContainerId, Container>();
+	
+	private static final Log LOG = LogFactory.getLog(ApplicationMaster.class);
+
+	@SuppressWarnings("unchecked")
+	void run() throws YarnException, IOException {
+
+		logInformation();
+		Configuration conf = new Configuration();
+
+		// 1. create amRMClient
+		
+		amRMClient = AMRMClientAsync.createAMRMClientAsync(
+				1000, new RMCallbackHandler());
+		amRMClient.init(conf);
+		amRMClient.start();
+		// 2. Create nmClientAsync
+		amNMClient = new NMClientAsyncImpl(new NMCallbackHandler());
+		amNMClient.init(conf);
+		amNMClient.start();
+
+		// 3. register with RM and this will heartbeating to RM
+		RegisterApplicationMasterResponse response = amRMClient
+				.registerApplicationMaster(NetUtils.getHostname(), -1, "");
+
+		// 4. Request containers
+		response.getContainersFromPreviousAttempts();
+		//int numContainers = 10;
+
+		for (int i = 0; i < numTotalContainers.get(); i++) {
+			ContainerRequest containerAsk = new ContainerRequest(
+					//100*10M + 1vcpu
+					Resource.newInstance(100, 1), null, null,
+					Priority.newInstance(0));
+			amRMClient.addContainerRequest(containerAsk);
+		}
+	}
+	
+	void waitComplete() throws YarnException, IOException{
+		while(numTotalContainers.get() != numCompletedConatiners.get()){
+			try{
+				Thread.sleep(1000);
+				LOG.info("waitComplete" + 
+					", numTotalContainers=" + numTotalContainers.get() +
+					", numCompletedConatiners=" + numCompletedConatiners.get());
+			} catch (InterruptedException ex){}
+		}
+		LOG.info("ShutDown exeService Start");
+		exeService.shutdown();
+		LOG.info("ShutDown exeService Complete");
+		amNMClient.stop();
+		LOG.info("amNMClient  stop  Complete");
+		amRMClient.unregisterApplicationMaster(FinalApplicationStatus.SUCCEEDED, "dummy Message", null);
+		LOG.info("unregisterApplicationMaster  Complete");
+		amRMClient.stop();
+		LOG.info("amRMClient  stop Complete");
+	}
+
+	void logInformation() {
+		System.out.println("This is System.out.println");
+		System.err.println("This is System.err.println");
+
+		String containerIdStr = System
+				.getenv(ApplicationConstants.Environment.CONTAINER_ID.name());
+
+		LOG.info("containerIdStr " + containerIdStr);
+
+		ContainerId containerId = ConverterUtils.toContainerId(containerIdStr);
+		ApplicationAttemptId appAttemptId = containerId
+				.getApplicationAttemptId();
+		LOG.info("appAttemptId " + appAttemptId.toString());
+	}
+
+	/*
+	 * 
+	 * UserGroupInformation ugi; final TimelineClient timelineClient;
+	 * 
+	 * TimelineEntity entity = new TimelineEntity();
+	 * entity.setEntityId(appAttemptId.toString());
+	 * entity.setEntityType(DSEntity.DS_APP_ATTEMPT.toString());
+	 * entity.setDomainId(domainId); entity.addPrimaryFilter("user",
+	 * ugi.getShortUserName()); TimelineEvent event = new TimelineEvent();
+	 * event.setEventType(appEvent.toString());
+	 * event.setTimestamp(System.currentTimeMillis()); entity.addEvent(event);
+	 * 
+	 * try{ ugi.doAs(new PrivilegedExceptionAction<TimelinePutResponse>(){
+	 * public TimelinePutResponse run() throws Exception { return
+	 * timelineClient.putEntities(entity); } }); } catch (Exception e){
+	 * LOG.error(e); }
+	 * 
+	 * }
+	 */
+
+	public static void main(String[] args) throws Exception {
+		ApplicationMaster am = new ApplicationMaster();
+		am.run();
+		am.waitComplete();
+	}
+}
---
+++ ctr/src/main/java/com/baidu/ctr/Client.java
@@ -0,0 +1,167 @@
+package com.baidu.ctr;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.io.FilenameUtils;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.security.UserGroupInformation;
+import org.apache.hadoop.util.ClassUtil;
+import org.apache.hadoop.yarn.api.ApplicationConstants;
+import org.apache.hadoop.yarn.api.ApplicationConstants.Environment;
+import org.apache.hadoop.yarn.api.protocolrecords.GetNewApplicationResponse;
+import org.apache.hadoop.yarn.api.records.ApplicationId;
+import org.apache.hadoop.yarn.api.records.ApplicationReport;
+import org.apache.hadoop.yarn.api.records.ApplicationSubmissionContext;
+import org.apache.hadoop.yarn.api.records.ContainerLaunchContext;
+import org.apache.hadoop.yarn.api.records.LocalResource;
+import org.apache.hadoop.yarn.api.records.LocalResourceType;
+import org.apache.hadoop.yarn.api.records.LocalResourceVisibility;
+import org.apache.hadoop.yarn.api.records.Priority;
+import org.apache.hadoop.yarn.api.records.Resource;
+import org.apache.hadoop.yarn.client.api.YarnClient;
+import org.apache.hadoop.yarn.client.api.YarnClientApplication;
+import org.apache.hadoop.yarn.conf.YarnConfiguration;
+import org.apache.hadoop.yarn.exceptions.YarnException;
+import org.apache.hadoop.yarn.util.ConverterUtils;
+import org.apache.log4j.Logger;
+
+public class Client {
+    static private Logger logger = Logger.getLogger(Client.class);
+
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        if (UserGroupInformation.isSecurityEnabled()) {
+            throw new Exception("SecurityEnabled , not support");
+        }
+
+        // 1. create and start a yarnClient
+        YarnClient yarnClient = YarnClient.createYarnClient();
+        yarnClient.init(conf);
+        yarnClient.start();
+
+        // 2. create an application
+        YarnClientApplication app = yarnClient.createApplication();
+        app.getApplicationSubmissionContext()
+                .setKeepContainersAcrossApplicationAttempts(false);
+        app.getApplicationSubmissionContext().setApplicationName(
+                "com.baidu.ctr.ApplicationMaster");
+
+        // 3. Set the app's resource usage, 100*10MB, 1vCPU
+        Resource capability = Resource.newInstance(1000, 1);
+        app.getApplicationSubmissionContext().setResource(capability);
+
+        // 4. Set the app's localResource env and command by
+        // ContainerLaunchContext
+        ContainerLaunchContext amContainer = createAMContainerLanunchContext(
+                conf, app.getApplicationSubmissionContext().getApplicationId());
+        app.getApplicationSubmissionContext().setAMContainerSpec(amContainer);
+
+
+        // 5. submit to queue default
+        app.getApplicationSubmissionContext().setPriority(
+                Priority.newInstance(0));
+        app.getApplicationSubmissionContext().setQueue("default");
+        ApplicationId appId = yarnClient.submitApplication(app
+                .getApplicationSubmissionContext());
+
+        monitorApplicationReport(yarnClient, appId);
+    }
+
+    private static ContainerLaunchContext createAMContainerLanunchContext(
+            Configuration conf, ApplicationId appId) throws IOException {
+        //Add this jar file to hdfs
+        Map<String, LocalResource> localResources = new HashMap<String, LocalResource>();
+        FileSystem fs = FileSystem.get(conf);
+        String thisJar = ClassUtil.findContainingJar(Client.class);
+        String thisJarBaseName = FilenameUtils.getName(thisJar);
+        logger.info("thisJar is " + thisJar);
+
+        addToLocalResources(fs, thisJar, thisJarBaseName, appId.toString(),
+                localResources);
+
+        //Set CLASSPATH environment
+        Map<String, String> env = new HashMap<String, String>();
+        StringBuilder classPathEnv = new StringBuilder(
+                Environment.CLASSPATH.$$());
+        classPathEnv.append(ApplicationConstants.CLASS_PATH_SEPARATOR);
+        classPathEnv.append("./*");
+        for (String c : conf
+                .getStrings(
+                        YarnConfiguration.YARN_APPLICATION_CLASSPATH,
+                        YarnConfiguration.DEFAULT_YARN_CROSS_PLATFORM_APPLICATION_CLASSPATH)) {
+            classPathEnv.append(ApplicationConstants.CLASS_PATH_SEPARATOR);
+            classPathEnv.append(c.trim());
+        }
+
+        if (conf.getBoolean(YarnConfiguration.IS_MINI_YARN_CLUSTER, false)) {
+            classPathEnv.append(':');
+            classPathEnv.append(System.getProperty("java.class.path"));
+        }
+        env.put(Environment.CLASSPATH.name(), classPathEnv.toString());
+
+        //Build the execute command
+        List<String> commands = new LinkedList<String>();
+        StringBuilder command = new StringBuilder();
+        command.append(Environment.JAVA_HOME.$$()).append("/bin/java  ");
+        command.append("-Dlog4j.configuration=container-log4j.properties ");
+        command.append("-Dyarn.app.container.log.dir=" +
+                ApplicationConstants.LOG_DIR_EXPANSION_VAR + " ");
+        command.append("-Dyarn.app.container.log.filesize=0 ");
+        command.append("-Dhadoop.root.logger=INFO,CLA ");
+        command.append("com.baidu.ctr.ApplicationMaster ");
+        command.append("1>" + ApplicationConstants.LOG_DIR_EXPANSION_VAR + "/stdout ");
+        command.append("2>" + ApplicationConstants.LOG_DIR_EXPANSION_VAR + "/stderr ");
+        commands.add(command.toString());
+
+        ContainerLaunchContext amContainer = ContainerLaunchContext
+                .newInstance(localResources, env, commands, null, null, null);
+        return amContainer;
+    }
+
+    private static void addToLocalResources(FileSystem fs, String fileSrcPath,
+                                            String fileDstPath, String appId,
+                                            Map<String, LocalResource> localResources)
+            throws IllegalArgumentException, IOException {
+        String suffix = "mytest" + "/" + appId + "/" + fileDstPath;
+        Path dst = new Path(fs.getHomeDirectory(), suffix);
+        logger.info("hdfs copyFromLocalFile " + fileSrcPath + " =>" + dst);
+        fs.copyFromLocalFile(new Path(fileSrcPath), dst);
+        FileStatus scFileStatus = fs.getFileStatus(dst);
+        LocalResource scRsrc = LocalResource.newInstance(
+                ConverterUtils.getYarnUrlFromPath(dst), LocalResourceType.FILE,
+                LocalResourceVisibility.APPLICATION, scFileStatus.getLen(),
+                scFileStatus.getModificationTime());
+
+        localResources.put(fileDstPath, scRsrc);
+    }
+
+    private static void monitorApplicationReport(YarnClient yarnClient, ApplicationId appId) throws YarnException, IOException {
+        while (true) {
+            try {
+                Thread.sleep(5 * 1000);
+            } catch (InterruptedException e) {
+
+            }
+            ApplicationReport report = yarnClient.getApplicationReport(appId);
+            logger.info("Got application report " + ", clientToAMToken="
+                    + report.getClientToAMToken() + ", appDiagnostics="
+                    + report.getDiagnostics() + ", appMasterHost="
+                    + report.getHost() + ", appQueue=" + report.getQueue()
+                    + ", appMasterRpcPort=" + report.getRpcPort()
+                    + ", appStartTime=" + report.getStartTime()
+                    + ", yarnAppState="
+                    + report.getYarnApplicationState().toString()
+                    + ", distributedFinalState="
+                    + report.getFinalApplicationStatus().toString()
+                    + ", appTrackingUrl=" + report.getTrackingUrl()
+                    + ", appUser=" + report.getUser());
+        }
+    }
+}
